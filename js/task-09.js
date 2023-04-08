596855b88699d268a51afaaef57e912fb7826785
Seven <graf.lancelot7@gmail.com>
2023-04-08 23:47:58 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,19 @@
+const refs = {
+  btnChangeColor: document.querySelector('.change-color'),
+  outputColor: document.querySelector('span.color')
+}
+
+refs.btnChangeColor.addEventListener('click', onBtnChangeColorClicl)
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function onBtnChangeColorClicl(event){
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  refs.outputColor.textContent = color;
+}
+
