4a1b705a3e9c8ac146eed628c265f5584122c7fa
Seven <graf.lancelot7@gmail.com>
2023-04-08 18:06:02 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,25 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const refs = {
+  ingredients: document.querySelector("#ingredients"),
+};
+
+const createItemIngredients = function (elements, nameItem) {
+  const items = [];
+  elements.forEach((element) => {
+    const item = document.createElement("li");
+    item.textContent = element;
+    item.classList.add(nameItem);
+    items.push(item);
+  });
+  return items;
+};
+
+refs.ingredients.append(...createItemIngredients(ingredients, "item"));
