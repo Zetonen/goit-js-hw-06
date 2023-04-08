e40040d7c883c2982285d0b6198eb57fd9afcc04
Seven <graf.lancelot7@gmail.com>
2023-04-08 23:42:00 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const refs = {
+  form: document.querySelector(".login-form"),
+};
 
+refs.form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const elements = event.currentTarget.elements;
+  const formData = new FormData(event.currentTarget);
+  const objFormValues = {};
+
+  if (!elements[0].value || !elements[1].value) {
+    alert("всі поля повинні бути заповнені");
+    return;
+  }
+
+  formData.forEach((value, name) => {
+    objFormValues[name] = value;
+  });
+  console.log(objFormValues);
+  event.currentTarget.reset();
+}
